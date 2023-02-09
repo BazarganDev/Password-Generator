ca27f89fcd23d4eff4b13b33cc8d710170b102ea
BazarganDev <124906353+BazarganDev@users.noreply.github.com>
2023-02-09 20:14:20 +0330

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -3,13 +3,14 @@ import random
 import string
 
 def GENERATE_PASSWORD():
+	""" Generate a password with combination of numbers, letters(uppercase & lowercase) and punctuations."""
 	NUMBERS = string.digits
 	ALPHABET = string.ascii_letters
 	PUNCTUATION = string.punctuation
 	SEQUENCE = NUMBERS + ALPHABET + PUNCTUATION
 
 	PASSWORD = ""
-
+	# Select 30 characters from the combined sequence
 	for character in range(30):
 		character = random.choice(SEQUENCE)
 		PASSWORD += character
